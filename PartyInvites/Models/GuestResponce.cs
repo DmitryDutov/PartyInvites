b792e8805d54fa8a3a1dc909f624679a23307b7e
DmitryDutov <dmitry_dutov@inbox.ru>
2021-09-12 00:55:47 +0300

    Добавление проверки достоверности + вывода ошибки при неверном вводе

--- PartyInvites/Models/GuestResponce.cs
+++ PartyInvites/Models/GuestResponce.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponce
     {
+        //Указание ФИО
+        [Required(ErrorMessage ="Please enter your name")]
         public string Name { get; set; }
+
+        //Указание email
+        [Required(ErrorMessage ="Please enter your email adress")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage ="Please enter your email adress")]
         public string Email { get; set; }
+
+        //Указание телефона
+        [Required(ErrorMessage ="Please enter your phone number")]
         public string Phone { get; set; }
+
+        //Решение о посещении
+        [Required(ErrorMessage ="Please specify whether you'll attend")]
         public bool? WillAttend { get; set; }
 
     }
